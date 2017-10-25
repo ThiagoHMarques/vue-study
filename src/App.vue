b89dd3743d670a593337815b2f062dc33d4471be
Thiago Marques <thiagohsmarques@gmail.com>
2017-10-25 14:04:30 -0200

    Aula 2 Video 4 O poder das diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <h1>{{ titulo }}</h1>
-        <img :src="foto.url" :alt="foto.titulo">
+        <ul>
+            <li v-for="foto of fotos">
+                <img :src="foto.url" :alt="foto.titulo">
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -10,10 +14,16 @@ export default {
   data() {
       return {
           titulo: 'Alurapic',
-          foto: {
-              url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-              titulo: 'Cachorro'
-          }
+          fotos: [
+            {
+                url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+                titulo: 'Cachorro'
+            },
+            {
+                url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+                titulo: 'Cachorrão'
+            }
+          ]
       }
   }
 }
